7d0717eb8e1922e164cd994cc5b918adeab79551
Nari Roh <nari.rohh@gmail.com>
2018-03-13 12:10:02 -0600

    UI part-2

---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchOpacity } from 'react-native';
+
+export default class ToDo extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
